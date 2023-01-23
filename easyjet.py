eefa697f5397c90b37aa684d3c1acf75aed6a85c
Big one <123197919+Aaslknc@users.noreply.github.com>
2023-01-23 15:40:39 -0300

    EASY JET

---
+++ easyjet.py
@@ -0,0 +1,9 @@
+largura = float(input())
+comprimento = float(input())
+altura = float(input())
+
+if largura <= 45 and comprimento <= 56 and altura <= 25:
+  print('PERMITIDA')
+
+if largura > 45 or comprimento > 56 or altura > 25:
+  print('PROIBIDA')
